c2823b0ba05bda92394ef40607cc05df8426f0e1
JoaquinArruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-02-08 18:44:57 -0300

    added basic files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('hey yaa!')
+// import "./style.css"
+
+header = document.createElement('div')
+header.classList.add('header')
+header.innerText = "asd"
+document.body.appendChild(header)
